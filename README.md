f44d223fe28f19341d7ae6e758e7f0996700eec6
matkamfonik <mat.kamfonik@gmail.com>
2023-01-03 22:45:59 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Workshop2
+Console program for Data Access Object
+
+## INTRODUCTION
+**Workshop 2** is a part of program that will be used to menage user data in DAO pattern.
+
+## FUNCTIONALITY
+This program comunicates business layer with database.
+
+**Workshop 2** 
++ __implements CRUD__, 
++ __implements findAll__ 
++ __implements deleteAll__.
---
+++ src/main/java/pl/coderslab/DbUtil.java
@@ -0,0 +1,75 @@
+package pl.coderslab;
+
+import java.sql.*;
+
+public class DbUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/products_ex?useSSL=false&characterEncoding=utf8&serverTimezone=UTC";
+    private static final String DB_URL2 = "jdbc:mysql://localhost:3306/cinemas_ex?useSSL=false&characterEncoding=utf8&serverTimezone=UTC";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = Password.PASSWORD;
+
+
+
+
+    public static Connection connectToProducts() throws SQLException {
+        return DriverManager.getConnection(
+                DB_URL,
+                DB_USER,
+                DB_PASS
+        );
+    }
+    public static Connection connectToCinemas() throws SQLException {
+        return DriverManager.getConnection(
+                DB_URL2,
+                DB_USER,
+                DB_PASS
+        );
+    }
+    public static void insert(Connection conn, String query, String... params) {
+        try ( PreparedStatement statement = conn.prepareStatement(query)) {
+            for (int i = 0; i < params.length; i++) {
+                statement.setString(i + 1, params[i]);
+            }
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printData(Connection conn, String query, String... columnNames) throws SQLException {
+
+        try (PreparedStatement statement = conn.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery();) {
+            while (resultSet.next()) {
+                for (String columnName : columnNames) {
+                    System.out.println(resultSet.getString(columnName));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static final String DELETE_QUERY = "DELETE FROM tableName where id = ?";
+
+    public static void remove(Connection conn, String tableName, int id) {
+        try (PreparedStatement statement =
+                     conn.prepareStatement(DELETE_QUERY.replace("tableName", tableName));) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void update(Connection conn, String query, int id, String... params) {
+        try ( PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setInt(params.length+1, id);
+            for (int i = 0; i < params.length; i++) {
+                statement.setString(i + 1, params[i]);
+            }
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
