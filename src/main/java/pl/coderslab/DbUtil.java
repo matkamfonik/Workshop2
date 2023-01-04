f73e8d6aea2b04e61b107e6bd0132924bda66bc8
matkamfonik <mat.kamfonik@gmail.com>
2023-01-04 23:11:41 +0100

    DbUtil cleaning part 1

--- src/main/java/pl/coderslab/DbUtil.java
+++ src/main/java/pl/coderslab/DbUtil.java
@@ -17,30 +17,7 @@ public class DbUtil {
                 DB_PASS
         );
     }
-    public static void insert(Connection conn, String query, String... params) {
-        try ( PreparedStatement statement = conn.prepareStatement(query)) {
-            for (int i = 0; i < params.length; i++) {
-                statement.setString(i + 1, params[i]);
-            }
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
-    public static void printData(Connection conn, String query, String... columnNames) throws SQLException {
-
-        try (PreparedStatement statement = conn.prepareStatement(query);
-             ResultSet resultSet = statement.executeQuery();) {
-            while (resultSet.next()) {
-                for (String columnName : columnNames) {
-                    System.out.println(resultSet.getString(columnName));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
     private static final String DELETE_QUERY = "DELETE FROM tableName where id = ?";
 
     public static void remove(Connection conn, String tableName, int id) {
