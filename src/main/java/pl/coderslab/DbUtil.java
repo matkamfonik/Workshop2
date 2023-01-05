b6a1c3917ae6b756f5f7003f683c093eddb26b22
matkamfonik <mat.kamfonik@gmail.com>
2023-01-05 22:40:52 +0100

    DbUtil finaly cleaned

--- src/main/java/pl/coderslab/DbUtil.java
+++ src/main/java/pl/coderslab/DbUtil.java
@@ -8,8 +8,6 @@ public class DbUtil {
     private static final String DB_PASS = Password.PASSWORD;
 
 
-
-
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
                 DB_URL,
@@ -17,51 +15,4 @@ public class DbUtil {
                 DB_PASS
         );
     }
-    public static void insert(Connection conn, String query, String... params) {
-        try ( PreparedStatement statement = conn.prepareStatement(query)) {
-            for (int i = 0; i < params.length; i++) {
-                statement.setString(i + 1, params[i]);
-            }
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void printData(Connection conn, String query, String... columnNames) throws SQLException {
-
-        try (PreparedStatement statement = conn.prepareStatement(query);
-             ResultSet resultSet = statement.executeQuery();) {
-            while (resultSet.next()) {
-                for (String columnName : columnNames) {
-                    System.out.println(resultSet.getString(columnName));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    private static final String DELETE_QUERY = "DELETE FROM tableName where id = ?";
-
-    public static void remove(Connection conn, String tableName, int id) {
-        try (PreparedStatement statement =
-                     conn.prepareStatement(DELETE_QUERY.replace("tableName", tableName));) {
-            statement.setInt(1, id);
-            statement.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public static void update(Connection conn, String query, int id, String... params) {
-        try ( PreparedStatement statement = conn.prepareStatement(query)) {
-            statement.setInt(params.length+1, id);
-            for (int i = 0; i < params.length; i++) {
-                statement.setString(i + 1, params[i]);
-            }
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
